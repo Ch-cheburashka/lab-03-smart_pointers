79c8c8505fde92c9b9cede1c96db1e1ceca176a7
Ch-cheburashka <m9056883999@gmail.com>
2023-04-13 18:23:16 +0300

    deleted _ptr and _count fields in SharedPtr

--- include/SharedPtr/SharedPtr.hpp
+++ include/SharedPtr/SharedPtr.hpp
@@ -4,164 +4,81 @@
 template<typename T>
 class SharedPtr {
 public:
-    SharedPtr() : _ptr(nullptr), _count(nullptr) {}
 
-    explicit SharedPtr(T *ptr) : _ptr(ptr), _count(new size_t(1)) { }
+    SharedPtr() : _block(nullptr) {}
+
+    explicit SharedPtr(T *ptr) : _block(new control_block<T>{1,*ptr}) {}
 
     SharedPtr(const SharedPtr &r) {
-        if (r._block != nullptr) {
-            _block = r._block;
-            ++_block->counter;
-        }
-        else {
-            _ptr = r._ptr;
-            _count = r._count;
-            ++*_count;
-        }
+        _block = r._block;
+        ++_block->counter;
     }
 
     SharedPtr(SharedPtr &&r) noexcept {
-        if (r._block != nullptr) {
-            _block = r._block;
-            r._block = nullptr;
-        }
-        else {
-            _ptr = r._ptr;
-            _count = r._count;
-            r._count = nullptr;
-            r._ptr = nullptr;
-        }
+        _block = r._block;
+        r._block = nullptr;
     }
 
     ~SharedPtr() {
-        if (_block != nullptr) {
-            if (_block->counter > 1)
-                --_block->counter;
-            else
-                delete _block;
-        }
+        if (_block->counter > 1)
+            --_block->counter;
         else {
-            if (_count != nullptr && *_count > 1)
-                --*_count;
-            else {
-                delete _ptr;
-                delete _count;
-            }
+            delete _block;
         }
     }
 
     auto operator=(const SharedPtr &r) -> SharedPtr & {
-        if (r._block != nullptr) {
-            _block = r._block;
-            ++_block->counter;
-        }
-        else {
-            _ptr = r._ptr;
-            _count = r._count;
-            ++*_count;
-        }
+        _block = r._block;
+        ++_block->counter;
         return *this;
     }
 
     auto operator=(SharedPtr &&r)  noexcept -> SharedPtr & {
-        if (r._block != nullptr) {
-            _block = r._block;
-            delete r._block;
-        }
-        else {
-            _ptr = r._ptr;
-            _count = r._count;
-            r._count = nullptr;
-            r._ptr = nullptr;
-            return *this;
-        }
+        _block = r._block;
+        delete r._block;
         return *this;
     }
 
     explicit operator bool() const {
-        return _ptr != nullptr || _block != nullptr;
+        return _block != nullptr;
     }
 
     auto operator*() const -> T & {
-        if (_block != nullptr) {
-            return _block->object;
-        }
-        return *_ptr;
+        return _block->object;
     }
 
     auto operator->() const -> T * {
-        if (_block != nullptr) {
-            return &_block->object;
-        }
-        return _ptr;
+        return &_block->object;
     }
 
     auto get() -> T * {
-        if (_block != nullptr) {
-            return *_block->object;
-        }
-        return _ptr;
+        return *_block->object;
     }
 
     void reset() {
         if (_block != nullptr)
             _block = nullptr;
-        else
-            _ptr = nullptr;
     }
 
     void reset(T *ptr) {
-        if (_block != nullptr) {
-            if (_block->counter == 1) {
-                delete _block;
-                _block->counter = 1;
-                _block->object = *ptr;
-            }
-            else {
-                _block = nullptr;
-                _ptr = ptr;
-                _count = new size_t(1);
-            }
+        if (_block->counter == 1) {
+            delete _block;
+            _block->counter = 1;
+            _block->object = *ptr;
         }
         else {
-            if (*_count == 1) {
-                delete _ptr;
-                _ptr = ptr;
-            } else {
-                _ptr = ptr;
-                *_count = 1;
-            }
+            --_block->counter;
+            _block = new control_block<T>{1,*ptr};
         }
     }
 
     void swap(SharedPtr &r) {
-        if (r._block != nullptr) {
-            if (_block != nullptr && _block != r._block) {
-                std::swap(_block,r._block);
-            }
-            else {
-                std::swap(_block,r._block);
-                std::swap(_ptr,r._ptr);
-                std::swap(_count,r._count);
-            }
-        }
-        else {
-            if (_ptr != nullptr && _ptr != r._ptr) {
-                std::swap(_ptr, r._ptr);
-                std::swap(_count, r._count);
-            }
-            else {
-                std::swap(_block,r._block);
-                std::swap(_ptr,r._ptr);
-                std::swap(_count,r._count);
-            }
-        }
+        if (_block != r._block)
+            std::swap(_block,r._block);
     }
 
     auto use_count() const -> size_t {
-        if (_block != nullptr)
-            return _block->counter;
-        return *_count;
+        return _block->counter;
     }
 private:
     template <typename U>
@@ -170,8 +87,6 @@ private:
         U object;
     };
 
-    T* _ptr = nullptr;
-    size_t* _count = nullptr;
     control_block<T>* _block = nullptr;
 
     explicit SharedPtr(control_block<T>* storage_ptr) : _block(storage_ptr) {}
